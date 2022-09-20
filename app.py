92e34db1ab1dd23bf277111eca2428c5b3f5f38c
예린 <yerin@yelin-ui-MacBookAir.local>
2022-09-20 19:53:41 +0900

    (add) api_register, api_login

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
 from flask import Flask, render_template, jsonify, request
-from pymongo import MongoClient     
-import hashlib
-      
-client = MongoClient('localhost', 27017)  
-db = client.dbsparta                    
+from pymongo import MongoClient  
+import hashlib, datetime, jwt
+
 
 app = Flask(__name__)
 
-## URL 별로 함수명이 같거나,
-## route('/') 등의 주h소가 같으면 안됩니다.
+client = MongoClient('localhost', 27017) 
+db = client.dbsparta  
 
 @app.route('/')
 def home():
    return render_template('signup.html')
 
+@app.route('/register', methods=['POST'])
+def api_register():
+   id_receive = request.form['id_give']
+   pw_receive = request.form['pw_give']
+   name_receive = request.form['name_give']
+   room_receive = request.form['room_give']
+
+   pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+   user = {'id': id_receive, 'pw': pw_hash, 'name': name_receive, 'room': room_receive}
+
+   db.users.insert_one(user)
+
+   return jsonify({'result': 'success'})
+   
+
+@app.route('/login', methods=['POST'])
+def api_login():
+   id_receive = request.form['id_give']
+   pw_receive = request.form['pw_give']
+
+   pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+   result = db.users.find_one({'id': id_receive, 'pw': pw_hash})
+
+   if result is not None:
+      payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)   
+      }
+      token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+      return jsonify({'result': 'success', 'token': token})
+   else:
+      return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
-
-@app.route('/api/register', methods=['POST'])
-def api_register():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-    nickname_receive = request.form['nickname_give']
-
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
-
-    return jsonify({'result': 'success'})
