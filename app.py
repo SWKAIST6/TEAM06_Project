5e972a8a4858d05dc9142d08cf19f5a1e8f0ca75
예린 <yerin@yelin-ui-MacBookAir.local>
2022-09-21 15:44:56 +0900

    yrp

--- app.py
+++ app.py
@@ -20,7 +20,31 @@ def getLogin():
 def getRegister():
    return render_template('signup.html')
 
-@app.route('/api/register', methods=['post'])
+@app.route('/main')
+def getMain():
+   return render_template('main.html')
+
+
+@app.route('/api/authentication', methods=['POST'])
+def api_authentication():
+   name_receive = request.form['name_give']
+   room_receive = request.form['room_give']
+   enter_receive = request.form['enter_give']
+   
+   print(name_receive)
+   print(room_receive)
+   print(enter_receive)
+   
+   user = {'name': name_receive, 'room': room_receive, 'enter_key': enter_receive}
+
+   result = db.junglers.find_one(user)
+
+   if result is not None:
+      return jsonify({'result': 'success'})
+   else:
+      return jsonify({'result': 'fail'})
+
+@app.route('/api/register', methods=['POST'])
 def api_register():
    id_receive = request.form['id_give']
    pw_receive = request.form['pw_give']
@@ -53,6 +77,49 @@ def api_login():
       return jsonify({'result': 'success', 'token': token})
    else:
       return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/market', methods=['POST'])
+def post_information():
+   datetime_objected = datetime.datetime.now()
+   datetime_spring = datetime_objected.strftime('%Y-%m-%d %H:%M:%S')
+   title_receive = request.form['title_give']  # 클라이언트로부터 title 받기
+   text_receive = request.form['text_give']  # 클라이언트로부터 text 받기
+   category_receive = request.form['category_give']  # 클라이언트로부터 category 받기
+
+   informations = list(db.informations.find({}, {'_id': 0}))
+   history = len(informations)
+   post_numbers = []
+   for i in range(history):
+      post_numbers.append(i)
+   if len(post_numbers) == 0:
+      max_post_number = 0
+   else:
+      max_post_number = max(post_numbers, default=0)+1
+
+   informations = {
+      'postnumber': max_post_number,
+      'datetime': datetime_spring,
+      'title': title_receive,
+      'text': text_receive,
+      'category': category_receive
+   }
+
+   # mongoDB에 데이터를 넣기
+   db.informations.insert_one(informations)
+   return jsonify({'result': 'success'})
+
+
+@app.route('/market', methods=['GET'])
+def show_information():
+   data = list(db.informations.find({}, {'_id': False}))
+   return render_template('main.html', informations=data)
+
+
+@app.route('/exchange/<postnumber>')
+def url_generator(postnumber):
+   data = list(db.informations.find({}, {'_id': False}))
+   return render_template('exchange.html', informations=data[int(postnumber)])
 
 
 
---
+++ jungler_db.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+client = MongoClient ('localhost', 27017)
+db = client.dbsparta
+
+while True:
+    jungler_name = input('이름을 입력해주세요: ')
+    jungler_room = input('호실 번호를 입력해주세요 e.g. 101: ')
+    jungler_enter = input('출입증 일련번호를 입력해주세요 e.g. D1111: ')
+
+    db.junglers.insert_one({'name': jungler_name, 'room': jungler_room, 'enter_key': jungler_enter})
+
+    print(jungler_room +'호 '+ jungler_name + ' jungler db에 추가되었습니다.\n')
